e1628b958942c1d0d28038fdf193542fbb5b5a2d
timothee ROY <timothee@vingtcinq.io>
2024-11-09 03:17:46 +0100

    back - add update profile

--- back/users/serializers.py
+++ back/users/serializers.py
@@ -5,6 +5,16 @@ from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(required=False, allow_blank=True)
+    last_name = serializers.CharField(required=False, allow_blank=True)
     class Meta:
         model = User
         fields = "__all__"
+        read_only_fields = ["email"]
+
+    def update(self, instance, validated_data):
+        for field, value in validated_data.items():
+            if value != "":
+                setattr(instance, field, value)
+        instance.save()
+        return instance
--- back/users/views.py
+++ back/users/views.py
@@ -71,11 +71,11 @@ class RetrieveUserView(APIView):
 class UpdateUserView(APIView):
     permission_classes = [IsAuthenticated]
     def put(self, request, pk):
-        user = User.objects.get(pk=pk)
-        serializer = UserSerializer(user, data=request.data)
+        user = request.user
+        serializer = UserSerializer(user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=200)
         return Response(serializer.errors, status=400)
 
 
