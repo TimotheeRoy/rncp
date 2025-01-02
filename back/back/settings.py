ea4836610da800723db06922bc77de09eb865b82
timothee ROY <timothee@vingtcinq.io>
2025-01-02 15:33:56 +0100

    back - update requirements

--- back/back/settings.py
+++ back/back/settings.py
@@ -85,8 +85,8 @@ WSGI_APPLICATION = "back.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
+        "NAME": os.getenv("POSTGRES_DB", "rncp"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST", ""),
         "PORT": os.getenv("POSTGRES_PORT"),
